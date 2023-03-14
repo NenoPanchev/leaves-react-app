c096f200cafb36e8e53c05109c53507b896dfdd8
neno <neno.panchev@gmail.com>
2023-03-14 09:16:21 +0200

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Home from './component/Home';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+import SignIn from './component/SignIn';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <SignIn />
   </React.StrictMode>
 );
 
