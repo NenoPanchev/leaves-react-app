163a9668193c3e26a74dd4b53aabb598312a1469
V.Kacharov <vladimir.kacharov@lightsoftbulgaria.com>
2023-04-21 12:05:16 +0300

    refactor duplicate code

--- src/components/calendar/CalendarBase.tsx
+++ src/components/calendar/CalendarBase.tsx
@@ -21,6 +21,7 @@ import HolidayService from '../../services/HolidayService';
 import RequestService from '../../services/RequestService';
 import BasicDialogAlert from '../Alert/BasicDialogAlert';
 import PdfFormRequest from '../pdfForm/PdfFormRequest';
+import { Day, disableWeekends } from './CalendarStyleComponent';
 
 dayjs.extend(isBetweenPlugin);
 export interface CalendarBaseRef {
@@ -28,180 +29,6 @@ export interface CalendarBaseRef {
 }
 type CustomDayProps = {
     onDateChange: (day: Dayjs | null) => void;
-}
-interface CustomPickerDayProps extends PickersDayProps<Dayjs> {
-    dayIsBetween: Array<boolean>;
-    isStart: Array<boolean>;
-    isEnd: Array<boolean>;
-    isRejected: Array<boolean | undefined>;
-    isRed: Array<boolean>;
-    requestDayIsHoliday: Array<boolean>;
-    isHoliday: Array<boolean>;
-}
-
-const CustomPickersDay = styled(PickersDay, {
-    shouldForwardProp: (prop) =>
-        prop !== 'dayIsBetween' &&
-        prop !== 'isStart' &&
-        prop !== 'isEnd' &&
-        prop !== 'isRejected' &&
-        prop !== 'isRed' &&
-        prop !== 'isBeforeToday' &&
-        prop !== 'isHoliday',
-})<CustomPickerDayProps>(({ theme, dayIsBetween, isStart, isEnd, isRejected, isRed, requestDayIsHoliday, isHoliday }) => {
-    let counter = 0;
-    let holidayCounter = 0;
-    let styl = {
-        borderRadius: 0,
-        borderTopRightRadius: '50%',
-        borderBottomRightRadius: '50%',
-        borderTopLeftRadius: '50%',
-        borderBottomLeftRadius: '50%',
-        backgroundColor: "#90caf9",
-        color: theme.palette.common.white,
-        '&:hover, &:focus': {
-            backgroundColor: '#42a5f5',
-        },
-
-    }
-
-    for (const dayIsBetweenItem of dayIsBetween) {
-        ////COLOR CHANGE
-
-
-        if (isRejected[counter] == false) {
-            //
-            //REJECTED
-            //
-            styl.backgroundColor = red[200];
-            styl['&:hover, &:focus'].backgroundColor = red[400];
-
-        } else if (isRejected[counter] == true) {
-            //
-            //APPROVED
-            //
-            if (!isRed[counter]) {
-                styl.backgroundColor = red[200];
-                styl['&:hover, &:focus'].backgroundColor = red[400];
-            }
-            else {
-                styl.backgroundColor = green[200];
-                styl['&:hover, &:focus'].backgroundColor = green[400];
-            }
-
-        }
-        else {
-            //
-            //NOT PROCESSED 
-            //
-            styl.backgroundColor = blue[200];
-            styl['&:hover, &:focus'].backgroundColor = blue[400];
-        }
-
-
-        if (requestDayIsHoliday[counter]) {
-            //HOLIDAY COLOR CHANGE
-            styl.backgroundColor = purple[500];
-            styl['&:hover, &:focus'].backgroundColor = purple[600];
-        }
-        //END OF COLOR CHANGE
-
-        //START OF FORM CHANGE
-        if (isStart[counter] && isEnd[counter]) {
-
-            return styl
-        } else if (isStart[counter]) {
-            styl.borderBottomRightRadius = '0%';
-            styl.borderTopRightRadius = '0%';
-            return styl
-
-        } else if (isEnd[counter]) {
-            styl.borderTopLeftRadius = '0%';
-            styl.borderBottomLeftRadius = '0%';
-            return styl;
-
-        } else if (dayIsBetweenItem) {
-
-            styl.borderTopLeftRadius = '0%';
-            styl.borderBottomLeftRadius = '0%';
-            styl.borderBottomRightRadius = '0%';
-            styl.borderTopRightRadius = '0%';
-            return styl;
-        }
-
-
-
-
-        counter++;
-    }
-    for (const holiday of isHoliday) {
-        if (holiday) {
-
-            //HOLIDAY
-            styl.backgroundColor = purple[500];
-            styl['&:hover, &:focus'].backgroundColor = purple[600];
-            return styl;
-        }
-
-        holidayCounter++;
-    }
-
-    return {}
-}) as React.ComponentType<CustomPickerDayProps>;
-let dayCounter = 0;
-
-function Day(props: PickersDayProps<Dayjs> & { requests?: Array<IRequestDataGet> } & { holidays?: Array<string> }) {
-    const { day, requests, holidays, ...other } = props;
-
-    if (requests == null) {
-        return <PickersDay day={day} {...other} />;
-    }
-    if (holidays == null) {
-        return <PickersDay day={day} {...other} />;
-    }
-    const dayIsBetween: Array<boolean> = [];
-    const isStart: Array<boolean> = [];
-    const isEnd: Array<boolean> = [];
-    const isRejected: Array<boolean | undefined> = [];
-    const isRed: Array<boolean> = [];
-    const requestDayIsHoliday: Array<boolean> = [];
-    const isHoliday: Array<boolean> = [];
-
-    holidays.forEach(holiday => {
-        //with WEEKENDS 
-        // isHoliday.push(day.isSame(holiday,'day')||day.day()==6||day.day()==0);
-
-        //without WEEKENDS 
-        isHoliday.push(day.isSame(holiday, 'day'));
-    })
-    requests.forEach(element => {
-
-        dayIsBetween.push(day.isBetween(element.startDate, element.endDate, null, '[]'));
-        isStart.push(day.isSame(element.startDate, 'day'));
-        isEnd.push(day.isSame(element.endDate, 'day'));
-        isRejected.push(element.approved)
-        isRed.push(day.isBetween(element.approvedStartDate, element.approvedEndDate, null, '[]'))
-        // isBeforeToday.push(day.isBefore(dayjs().subtract(1, 'day')))
-        console.log(holidays);
-        requestDayIsHoliday.push(holidays.includes(day.format("YYYY-MM-DD")))
-
-
-    }
-    );
-    return (
-        <CustomPickersDay
-            {...other}
-            day={day}
-            disableMargin
-            dayIsBetween={dayIsBetween}
-            isStart={isStart}
-            isEnd={isEnd}
-            isRejected={isRejected}
-            isRed={isRed}
-            requestDayIsHoliday={requestDayIsHoliday}
-            isHoliday={isHoliday}
-        />
-    );
 }
 
 const alertMassage = "You can not download Pdf of a request that is not approved!";
@@ -275,10 +102,6 @@ const CustomDay = (props: CustomDayProps, ref: React.ForwardedRef<CalendarBaseRe
         })
         props.onDateChange(newValue);
     };
-    const handleMonthChange = () => {
-        dayCounter = 0;
-
-    };
 
     const retrivePage = async () => {
         await RequestService.getAllByUser()
@@ -298,9 +121,6 @@ const CustomDay = (props: CustomDayProps, ref: React.ForwardedRef<CalendarBaseRe
                 console.log(e);
             });
     }
-    function disableWeekends(date: dayjs.Dayjs) {
-        return date.day() === 0 || date.day() === 6 || date.isBefore(dayjs().subtract(1, 'day'));
-    }
     return (
         <Grid item>
             <AlertMemo message={alertMassage} open={openAlert} onClose={updateAlertOpen}></AlertMemo>
@@ -318,8 +138,6 @@ const CustomDay = (props: CustomDayProps, ref: React.ForwardedRef<CalendarBaseRe
                                     holidays: holidays
                                 } as any,
                             }}
-
-                            onMonthChange={() => handleMonthChange()}
                             shouldDisableDate={disableWeekends}
                             onChange={(newValue) => handleChange(newValue)}
                         />
