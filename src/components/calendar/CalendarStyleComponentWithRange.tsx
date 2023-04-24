d41611b35a07fe5c91d0c8cad9d7e12e3cfa6ee0
V.Kacharov <vladimir.kacharov@lightsoftbulgaria.com>
2023-04-24 10:17:14 +0300

    fix range picker staying red

--- src/components/calendar/CalendarStyleComponentWithRange.tsx
+++ src/components/calendar/CalendarStyleComponentWithRange.tsx
@@ -12,12 +12,12 @@ interface CustomPickerDayRangeProps extends PickersDayProps<Dayjs> {
     isStart: Array<boolean>;
     isEnd: Array<boolean>;
     isRejected: Array<boolean | undefined>;
-    isRed: Array<boolean>;
+    isRed: Array<boolean | null>;
     requestdayisholiday: Array<boolean>;
     isHoliday: Array<boolean>;
     dayIsBetweenRange: boolean;
-    isFirstDayOfRange : boolean;
-    isLastDayOfRange:boolean;
+    isFirstDayOfRange: boolean;
+    isLastDayOfRange: boolean;
 }
 const CustomPickerDayRange = styled(PickersDay, {
     shouldForwardProp: (prop) =>
@@ -27,14 +27,14 @@ const CustomPickerDayRange = styled(PickersDay, {
         prop !== 'isRejected' &&
         prop !== 'isRed' &&
         prop !== 'isBeforeToday' &&
-        prop !== 'isHoliday'&&
+        prop !== 'isHoliday' &&
         prop !== 'dayIsBetweenRange' &&
         prop !== 'isFirstDayOfRange' &&
         prop !== 'isLastDayOfRange',
 })<CustomPickerDayRangeProps>(({ theme,
-     dayIsBetween, isStart, isEnd, isRejected, isRed,
-      requestdayisholiday: requestDayIsHoliday, isHoliday,
-      dayIsBetweenRange,isFirstDayOfRange,isLastDayOfRange}) => {
+    dayIsBetween, isStart, isEnd, isRejected: isApproved, isRed: notRed,
+    requestdayisholiday: requestDayIsHoliday, isHoliday,
+    dayIsBetweenRange, isFirstDayOfRange, isLastDayOfRange }) => {
     let counter = 0;
     let holidayCounter = 0;
     let styl = {
@@ -53,102 +53,111 @@ const CustomPickerDayRange = styled(PickersDay, {
     styl.backgroundColor = blue[100];
     styl['&:hover, &:focus'].backgroundColor = blue[200];
 
-    for (const dayIsBetweenItem of dayIsBetween) {
-        ////COLOR CHANGE
+    if (dayIsBetweenRange) {
+        //RANGE PICKER FORM CHANGE    
+        if (isFirstDayOfRange && isLastDayOfRange) {
+            return styl
+        } else if (isFirstDayOfRange) {
 
+            styl.borderBottomRightRadius = '0%';
+            styl.borderTopRightRadius = '0%';
+            return styl
+        } else if (isLastDayOfRange) {
 
-        if (isRejected[counter] == false) {
-            //
-            //REJECTED
-            //
-            styl.backgroundColor = red[200];
-            styl['&:hover, &:focus'].backgroundColor = red[300];
+            styl.borderTopLeftRadius = '0%';
+            styl.borderBottomLeftRadius = '0%';
+            return styl;
+        } else if (dayIsBetweenRange) {
 
-        } else if (isRejected[counter] == true) {
-            //
-            //APPROVED
-            //
-            if (!isRed[counter]) {
+            styl.borderTopLeftRadius = '0%';
+            styl.borderBottomLeftRadius = '0%';
+            styl.borderBottomRightRadius = '0%';
+            styl.borderTopRightRadius = '0%';
+            return styl;
+        }
+
+        //END RANGE PICKER FORM CHANGE
+    } else if (dayIsBetween) {
+        console.log("dayIsBetween")
+        for (const dayIsBetweenItem of dayIsBetween) {
+            ////COLOR CHANGE
+            if (isApproved[counter] === false) {
+                //
+                //REJECTED
+                //
                 styl.backgroundColor = red[200];
                 styl['&:hover, &:focus'].backgroundColor = red[300];
+
+            } else if (isApproved[counter] === true) {
+                //
+                //APPROVED
+                //
+
+                if (notRed[counter]) {
+                    styl.backgroundColor = green[200];
+                    styl['&:hover, &:focus'].backgroundColor = green[300];
+                }
+                else if (notRed[counter] === false) {
+                    console.log("red")
+                    styl.backgroundColor = red[200];
+                    styl['&:hover, &:focus'].backgroundColor = red[300];
+                }
+                else if (notRed[counter] === null) {
+                    console.log("blue")
+                    styl.backgroundColor = blue[100];
+                    styl['&:hover, &:focus'].backgroundColor = blue[200];
+                }
+
             }
             else {
-                styl.backgroundColor = green[200];
-                styl['&:hover, &:focus'].backgroundColor = green[300];
+                //
+                //NOT PROCESSED 
+                //
+                styl.backgroundColor = blue[200];
+                styl['&:hover, &:focus'].backgroundColor = blue[300];
             }
 
+
+            if (requestDayIsHoliday[counter]) {
+                //HOLIDAY COLOR CHANGE
+                styl.backgroundColor = purple[300];
+                styl['&:hover, &:focus'].backgroundColor = purple[400];
+            }
+            //END OF COLOR CHANGE
+
+
+
+
+            //START OF FORM CHANGE
+            if (isStart[counter] && isEnd[counter]) {
+
+                return styl
+            } else if (isStart[counter]) {
+                styl.borderBottomRightRadius = '0%';
+                styl.borderTopRightRadius = '0%';
+                return styl
+
+            } else if (isEnd[counter]) {
+                styl.borderTopLeftRadius = '0%';
+                styl.borderBottomLeftRadius = '0%';
+                return styl;
+
+            } else if (dayIsBetweenItem) {
+
+                styl.borderTopLeftRadius = '0%';
+                styl.borderBottomLeftRadius = '0%';
+                styl.borderBottomRightRadius = '0%';
+                styl.borderTopRightRadius = '0%';
+                return styl;
+            }
+
+
+
+
+            counter++;
         }
-        else {
-            //
-            //NOT PROCESSED 
-            //
-            styl.backgroundColor = blue[200];
-            styl['&:hover, &:focus'].backgroundColor = blue[300];
-        }
-
-
-        if (requestDayIsHoliday[counter]) {
-            //HOLIDAY COLOR CHANGE
-            styl.backgroundColor = purple[300];
-            styl['&:hover, &:focus'].backgroundColor = purple[400];
-        }
-        //END OF COLOR CHANGE
-
-
- 
-
-        //START OF FORM CHANGE
-        if (isStart[counter] && isEnd[counter]) {
-
-            return styl
-        } else if (isStart[counter]) {
-            styl.borderBottomRightRadius = '0%';
-            styl.borderTopRightRadius = '0%';
-            return styl
-
-        } else if (isEnd[counter]) {
-            styl.borderTopLeftRadius = '0%';
-            styl.borderBottomLeftRadius = '0%';
-            return styl;
-
-        } else if (dayIsBetweenItem) {
-
-            styl.borderTopLeftRadius = '0%';
-            styl.borderBottomLeftRadius = '0%';
-            styl.borderBottomRightRadius = '0%';
-            styl.borderTopRightRadius = '0%';
-            return styl;
-        }
-
-
-
-
-        counter++;
     }
-       //RANGE PICKER FORM CHANGE    
-     if (isFirstDayOfRange && isLastDayOfRange){
-     return styl 
-    }else if (isFirstDayOfRange){
-
-        styl.borderBottomRightRadius = '0%';
-        styl.borderTopRightRadius = '0%';
-        return styl 
-    }else if (isLastDayOfRange){
-        
-
-        styl.borderTopLeftRadius = '0%';
-        styl.borderBottomLeftRadius = '0%';
-        return styl;
-    }else  if(dayIsBetweenRange)
-    {
- 
-        styl.borderTopLeftRadius = '0%';
-        styl.borderBottomLeftRadius = '0%';
-        styl.borderBottomRightRadius = '0%';
-        styl.borderTopRightRadius = '0%';
-        return styl;
-    }
-    //END RANGE PICKER FORM CHANGE
+    
     for (const holiday of isHoliday) {
         if (holiday) {
 
@@ -162,7 +171,7 @@ const CustomPickerDayRange = styled(PickersDay, {
     }
 
 
-    
+
 
     return {}
 }) as React.ComponentType<CustomPickerDayRangeProps>;
@@ -171,63 +180,71 @@ const CustomPickerDayRange = styled(PickersDay, {
 
 
 export function DayWithRange(props: PickersDayProps<Dayjs> &
-    { requests?: Array<IRequestDataGet> } &
-    { holidays?: Array<string> } &
-    { endDate?: Dayjs | null } &
-    { startDate?: Dayjs | null }
-    ) {
-    
-        const { day, requests,startDate,endDate, holidays, ...other } = props;
-    
-        if (requests == null) {
-            return <PickersDay day={day} {...other} />;
-        }
-        if (holidays == null) {
-            return <PickersDay day={day} {...other} />;
-        }
-        if (endDate == null) {
-            return <PickersDay day={day} {...other} />;
-        }
-        if (startDate == null) {
-            return <PickersDay day={day} {...other} />;
-        }
+{ requests?: Array<IRequestDataGet> } &
+{ holidays?: Array<string> } &
+{ endDate?: Dayjs | null } &
+{ startDate?: Dayjs | null }
+) {
 
-        const dayIsBetween: Array<boolean> = [];
-        const isStart: Array<boolean> = [];
-        const isEnd: Array<boolean> = [];
-        const isRejected: Array<boolean | undefined> = [];
-        const isRed: Array<boolean> = [];
-        const requestDayIsHoliday: Array<boolean> = [];
-        const isHoliday: Array<boolean> = [];
+    const { day, requests, startDate, endDate, holidays, ...other } = props;
 
-   
-        holidays.forEach(holiday => {
-            //with WEEKENDS 
-            // isHoliday.push(day.isSame(holiday,'day')||day.day()==6||day.day()==0);
-    
-            //without WEEKENDS 
-            isHoliday.push(day.isSame(holiday, 'day'));
-        })
-        requests.forEach(element => {
-    
-            dayIsBetween.push(day.isBetween(element.startDate, element.endDate, null, '[]'));
-            isStart.push(day.isSame(element.startDate, 'day'));
-            isEnd.push(day.isSame(element.endDate, 'day'));
-            isRejected.push(element.approved)
+    if (requests == null) {
+        return <PickersDay day={day} {...other} />;
+    }
+    if (holidays == null) {
+        return <PickersDay day={day} {...other} />;
+    }
+    if (endDate == null) {
+        return <PickersDay day={day} {...other} />;
+    }
+    if (startDate == null) {
+        return <PickersDay day={day} {...other} />;
+    }
+
+    const dayIsBetween: Array<boolean> = [];
+    const isStart: Array<boolean> = [];
+    const isEnd: Array<boolean> = [];
+    const isRejected: Array<boolean | undefined> = [];
+    const isRed: Array<boolean | null> = [];
+    const requestDayIsHoliday: Array<boolean> = [];
+    const isHoliday: Array<boolean> = [];
+
+
+    holidays.forEach(holiday => {
+        //with WEEKENDS 
+        // isHoliday.push(day.isSame(holiday,'day')||day.day()==6||day.day()==0);
+
+        //without WEEKENDS 
+        isHoliday.push(day.isSame(holiday, 'day'));
+    })
+    requests.forEach(element => {
+
+        dayIsBetween.push(day.isBetween(element.startDate, element.endDate, null, '[]'));
+        isStart.push(day.isSame(element.startDate, 'day'));
+        isEnd.push(day.isSame(element.endDate, 'day'));
+        isRejected.push(element.approved)
+        if (element.approved) {
             isRed.push(day.isBetween(element.approvedStartDate, element.approvedEndDate, null, '[]'))
-            // isBeforeToday.push(day.isBefore(dayjs().subtract(1, 'day')))
-            requestDayIsHoliday.push(holidays.includes(day.format("YYYY-MM-DD")))
         }
-        );
+        else {
+            isRed.push(null);
+        }
 
-        const dayIsBetweenRange = day.isBetween(startDate, endDate, null, '[]');
-        const isFirstDayOfRange = day.isSame(startDate, 'day');
-        const isLastDayOfRange = day.isSame(endDate, 'day');
-        return (
-            <CustomPickerDayRange
+
+        // isBeforeToday.push(day.isBefore(dayjs().subtract(1, 'day')))
+        requestDayIsHoliday.push(holidays.includes(day.format("YYYY-MM-DD")))
+    }
+    );
+    console.log(isRejected)
+    console.log(isRed)
+    const dayIsBetweenRange = day.isBetween(startDate, endDate, null, '[]');
+    const isFirstDayOfRange = day.isSame(startDate, 'day');
+    const isLastDayOfRange = day.isSame(endDate, 'day');
+    return (
+        <CustomPickerDayRange
             dayIsBetweenRange={dayIsBetweenRange}
             isFirstDayOfRange={isFirstDayOfRange}
-            isLastDayOfRange={isLastDayOfRange} 
+            isLastDayOfRange={isLastDayOfRange}
             {...other}
             day={day}
             disableMargin
@@ -237,9 +254,9 @@ export function DayWithRange(props: PickersDayProps<Dayjs> &
             isRejected={isRejected}
             isRed={isRed}
             requestdayisholiday={requestDayIsHoliday}
-            isHoliday={isHoliday}            />
-        );
-    }
+            isHoliday={isHoliday} />
+    );
+}
 export function disableWeekends(date: dayjs.Dayjs) {
     return date.day() === 0 || date.day() === 6 || date.isBefore(dayjs().subtract(1, 'day'));
 }
