22b64abd3e29e69da91c9946052b41eaf73d0c36
V.Kacharov <vladimir.kacharov@lightsoftbulgaria.com>
2023-07-25 14:13:01 +0300

    fix login message on bad credentials

--- src/services/authService.tsx
+++ src/services/authService.tsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { axiosInstance as axios} from '../config/AxiosConfig';
+import { axiosInstance as axios } from '../config/AxiosConfig';
 import { formToJSON } from 'axios';
 import { LOGIN_URL } from '../constants/GlobalConstants';
 import AuthContext from '../contexts/AuthContext';
@@ -10,14 +10,14 @@ import { UserDetails } from '../models/objects/UserDetails';
 //TODO USER NOT REFRESHED ON LOGIN!!!!
 
 export const useLogin = () => {
-  var {user, setUser} = useContext(AuthContext);
+  var { user, setUser } = useContext(AuthContext);
   const navigate = useNavigate();
   const userDetails = new UserDetails();
   let serverResponse = '';
 
   const authenticate = async (userForm: FormData) => {
 
-     await axios.post(LOGIN_URL, formToJSON(userForm))
+    await axios.post(LOGIN_URL, formToJSON(userForm))
       .then((response) => {
         const token = response.data.jwt;
         userDetails.setEmail(response.data.email);
@@ -27,28 +27,38 @@ export const useLogin = () => {
 
         localStorage.setItem("SavedToken", 'Bearer ' + token);
         localStorage.setItem("Authenticated", 'true');
-        setUser(userDetails);  
+        setUser(userDetails);
         navigate('/')
       })
       .catch(error => {
         console.log(error)
-        serverResponse = 'No connection to the server';          
+        if (error.code === "ERR_NETWORK") {
+          serverResponse = 'No connection to the server'
+
+        } else if (error.response.status >= 500) {
+          serverResponse = 'No connection to the server';
+
+        } else {
+          serverResponse = error.response.data;
+
+        }
+
       })
-      return serverResponse;
+    return serverResponse;
   }
   return authenticate;
 }
 
 export const useRefresh = () => {
-  var {user, setUser} = useContext(AuthContext);
+  var { user, setUser } = useContext(AuthContext);
   // const navigate = useNavigate();
   // const path = useLocation().pathname;
   const userDetails = new UserDetails();
 
   const refreshUser = async () => {
     const jwt = localStorage.getItem('SavedToken')?.replaceAll('Bearer ', '');
-    
-     await axios.post(LOGIN_URL + '/refresh', {jwt: jwt})
+
+    await axios.post(LOGIN_URL + '/refresh', { jwt: jwt })
       .then((response) => {
         userDetails.setEmail(response.data.email);
         userDetails.setAuthorities(response.data.authorities);
@@ -58,7 +68,7 @@ export const useRefresh = () => {
       })
       .catch(error => console.log(error)
       )
-      return(userDetails);
+    return (userDetails);
   }
   return refreshUser;
 }
@@ -66,7 +76,7 @@ export const useRefresh = () => {
 export const LogOut = () => {
   const navigate = useNavigate();
   var { setUser } = useContext(AuthContext);
-  
+
   localStorage.setItem("SavedToken", '');
   useEffect(() => {
     setUser(null);
