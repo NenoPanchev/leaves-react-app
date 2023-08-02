70099cacbe894fe0db36297598b5b0da3b56c932
Neno Panchev <neno.panchev@lightsoftbulgaria.com>
2023-08-02 14:06:32 +0300

    added pagination to table

--- src/pages/dashboard/HistoryTable.tsx
+++ src/pages/dashboard/HistoryTable.tsx
@@ -1,15 +1,29 @@
-import { TableCell, TableContainer, Paper, Table, TableHead, TableRow, TableBody, Grid } from '@mui/material';
+import { TableCell, TableContainer, Paper, Table, TableHead, TableRow, TableBody, Grid, IconButton } from '@mui/material';
 import { t } from 'i18next';
 import { useState } from 'react';
 import { IDaysUsedByMonth } from '../../models/interfaces/request/IDaysUsedByMonth';
 import RequestService from '../../services/RequestService';
 import './HistoryTable.css'
+import { ChevronLeft, ChevronRight } from '@mui/icons-material';
 
 
 
 export default function AllEmployeesHistory() {
     const [history, setHistory] = useState<IDaysUsedByMonth[]>([]);
-    RequestService.getAllInTableView(2023)
+    const currentYear = new Date().getFullYear();
+    const minYear = 2022;
+    const maxYear = new Date().getMonth() === 11 ? currentYear + 1 : currentYear;
+    const [year, setYear] = useState(currentYear);
+
+    const handlePrevYear = () => {
+        setYear((prevYear) => Math.max(prevYear - 1, minYear));
+    };
+
+    const handleNextYear = () => {
+        setYear((prevYear) => Math.min(prevYear + 1, maxYear));
+    };
+
+    RequestService.getAllInTableView(year)
         .then((response: any) => {
             setHistory(response.data);
         })
@@ -52,12 +66,20 @@ export default function AllEmployeesHistory() {
         <Grid direction={'row'} width={'90%'}>
             <TableContainer component={Paper}>
                 <Table aria-label="vacation grid" className='font-size'
-                sx={{ 
-                    minWidth: 800
+                    sx={{
+                        minWidth: 800
                     }} >
                     <TableHead className='font-bold'>
                         <TableRow>
-                            <TableCell className='border-bottom' />
+                            <TableCell align='center' className='border-bottom'>
+                                <IconButton onClick={handlePrevYear} disabled={year <= minYear}>
+                                    <ChevronLeft />
+                                </IconButton>
+                                {year}
+                                <IconButton onClick={handleNextYear} disabled={year >= maxYear}>
+                                    <ChevronRight />
+                                </IconButton>
+                            </TableCell>
                             {history.map((employee) => (
                                 <TableCell key={`vacations-header-${employee.name}`}
                                     align='center'
